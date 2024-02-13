24520948bafdf04a07004f6eb53253c9d97a9f72
Abdiwali-eid <cabdiwalibashir@gmail.com>
2024-02-13 10:19:21 +0300

    herotext

--- web/src/styles/typography/SectionTitleStyles.js
+++ web/src/styles/typography/SectionTitleStyles.js
@@ -4,6 +4,7 @@ export const SectionTitleStyles = styled.h2`
   font-size: 3rem;
   font-family: 'Poppins', sans-serif;
   font-weight: 600;
+  margin-left: 10px;
   color: var(--primary);
   @media only screen and (max-width: 768px) {
     font-size: 2.5rem;
